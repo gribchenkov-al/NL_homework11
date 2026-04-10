e4e5d341c06a74f1d7e2d8302cb2455bceb59b71
Alexey Gribchenkov <gribchenkov@net-watch.ru>
2026-04-10 19:41:26 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -17,7 +17,7 @@ public class Main {
 
     }
 
-    public static void main() {
+    public static void main(String[] args) {
         Address addressOne = new Address("Россия", "Пенза");
         Address addressTwo = new Address("Россия", "Санкт-Петербург");
         Address addressThree = new Address("Япония", "Токио");
